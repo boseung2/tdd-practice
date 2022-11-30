c46befd08ce20bdb21ed159577271f7d29223424
정보승 <jungbs3726@naver.com>
2022-12-01 03:03:57 +0900

    Refactor: PasswordStrengthMeterTest 함수명 변경

--- src/test/java/org/tdd/tddpractice/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/org/tdd/tddpractice/chap02/PasswordStrengthMeterTest.java
@@ -15,32 +15,28 @@ public class PasswordStrengthMeterTest {
 
     private void assertStrength(String password, PasswordStrength expStr) {
         PasswordStrength result = meter.meter(password);
-        assertThat(result).isEqualTo((expStr));
+        assertThat(result).isEqualTo(expStr);
     }
 
     @Test
-    @DisplayName("암호가 모든 조건을 충족하면 암호 강도는 강함이여야 함")
-    void test1() {
+    void 암호가_모든_조건을_충족하면_암호_강도는_강함() {
         assertStrength("ab12!@AB", PasswordStrength.STRONG);
         assertStrength("abc1!Add", PasswordStrength.STRONG);
     }
 
     @Test
-    @DisplayName("길이가 8 미만이고 나머지 조건은 충족")
-    void test2() {
+    void 길이가_8미만이고_나머지_조건은_충족() {
         assertStrength("ab12!@A", PasswordStrength.NORMAL);
         assertStrength("Ab12!c", PasswordStrength.NORMAL);
     }
 
     @Test
-    @DisplayName("숫자를 포함하지 않고 나머지 조건은 충족")
-    void test3() {
+    void 숫자를_포함하지_않고_나머지_조건은_충족() {
         assertStrength("ab!ABqwert", PasswordStrength.NORMAL);
     }
 
     @Test
-    @DisplayName("유효하지 않은 암호")
-    void test4() {
+    void 유효하지_않은_암호() {
        assertStrength(null, PasswordStrength.INVALID);
     }
 
